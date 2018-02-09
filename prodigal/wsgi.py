ddadba5c71f7d2f20a890a8cc3e10cb7e512858c
Wonwoo Seo <wonwooseo@hotmail.com>
2018-02-09 14:33:09 -0600

    Django deployment test

--- prodigal/wsgi.py
+++ prodigal/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for cs411project project.
+WSGI config for prodigal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs411project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prodigal.settings")
 
 application = get_wsgi_application()
