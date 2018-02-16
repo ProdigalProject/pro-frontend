e1f706bf1c7af037a479db6911e1a570e620bb31
baskarn2 <baskarn2@illinois.edu>
2018-02-16 01:03:27 -0600

    adding tests

--- prodigal_app/tests.py
+++ prodigal_app/tests.py
@@ -3,23 +3,34 @@ from django.test import TestCase
 # Create your tests here.
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+
 
 class TestSignup(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        firefox_capabilities = DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = True
+        firefox_capabilities['binary'] = '/usr/bin/firefox'
+        self.driver = webdriver.Firefox(capabilities=firefox_capabilities)
 
-    def test_signup_fire(self):
+    def test_homepage_rendering(self):
         self.driver.get("https://prodigal-beta.azurewebsites.net/")
-        assert "Welcome to Prodigal!" in driver.title
-        '''
-        self.driver.find_element_by_id('title'). ("Welcome to Prodigal!")
-        self.driver.find_element_by_id('id_body').send_keys("test body")
-        self.driver.find_element_by_id('submit').click()
-        self.assertIn("http://localhost:8000/", self.driver.current_url)
-		'''
+        assert "Welcome to Prodigal!" in self.driver.title
+
+    def test_logo_return_to_homepage(self):
+        self.driver.get("https://prodigal-beta.azurewebsites.net/")
+        elem = self.driver.find_element_by_id('navbar_login')
+        elem.click()
+        elem = self.driver.find_element_by_id('navbar_logo')
+        elem.click()
+        assert "Welcome to Prodigal!" in self.driver.title
+
     def tearDown(self):
         self.driver.quit
+
 
 if __name__ == '__main__':
     unittest.main()
