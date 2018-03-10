7336152914b8814bd61b8c1597fdea4af7b6c3ee
EC2 Default User <baskarn2@illinois.edu>
2018-03-10 21:03:10 +0000

    updating test for new features

--- prodigal_app/tests.py
+++ prodigal_app/tests.py
@@ -31,21 +31,88 @@ class TestSignup(unittest.TestCase):
         elem.click()
         assert "Welcome to Prodigal!" in self.driver.title
     
-    def test_search_bar(self):
-        elem = self.driver.find_element_by_xpath("//a[contains(text(),'Secret backdoor to profile page')]")
+    def test_login_success(self):
+        elem = self.driver.find_element_by_id('navbar_login')
+        #elem = self.driver.find_element_by_xpath("//a[contains(text(),'Secret backdoor to profile page')]")
         elem.click()
+        elem = self.driver.find_element_by_class_name('w3-input w3-border w3-margin-bottom')
+        elem.send_keys('test')
+        
+        elem = self.driver.find_element_by_class_name('w3-input w3-border')
+        elem.send_keys('pw')
+        
+        elem = self.driver.find_element_by_id('submit')
+        elem.click() 
+        
+        elem = self.driver.find_element_by_id('profile_username')
+        assert elem.text == 'test'
+        
+    def test_login_epic_fail(self):
+        elem = self.driver.find_element_by_id('navbar_login')
+        #elem = self.driver.find_element_by_xpath("//a[contains(text(),'Secret backdoor to profile page')]")
+        elem.click()
+        elem = self.driver.find_element_by_class_name('w3-input w3-border w3-margin-bottom')
+        elem.send_keys('test')
+        
+        elem = self.driver.find_element_by_class_name('w3-input w3-border')
+        elem.send_keys('pwd')
+        
+        elem = self.driver.find_element_by_id('submit')
+        elem.click() 
+        
+        elem = self.driver.find_element_by_id('login_fail_msg')
+        assert elem.text == 'Login Failed!'    
+    
+    def test_search_bar_on_success(self):
+        elem = self.driver.find_element_by_id('navbar_login')
+        #elem = self.driver.find_element_by_xpath("//a[contains(text(),'Secret backdoor to profile page')]")
+        elem.click()
+        elem = self.driver.find_element_by_class_name('w3-input w3-border w3-margin-bottom')
+        elem.send_keys('test')
+        
+        elem = self.driver.find_element_by_class_name('w3-input w3-border')
+        elem.send_keys('pw')
+        
+        elem = self.driver.find_element_by_id('submit')
+        elem.click() 
+        
         elem = self.driver.find_element_by_id('navbar_searchbox')
         elem.send_keys('aapl')
-        elem.submit() 
+        elem.submit()
+        
+        
         elem = self.driver.find_element_by_id('company_name')
         assert elem.text == 'Apple Inc.'
         elem = self.driver.find_element_by_id('company_description')
         assert elem.text
         elem = self.driver.find_element_by_id('news')
         assert elem.text
+        
+        
         # test exception handle
 
         # test api status
+    
+    def test_search_bar_on_epic_fail(self):
+        elem = self.driver.find_element_by_id('navbar_login')
+        #elem = self.driver.find_element_by_xpath("//a[contains(text(),'Secret backdoor to profile page')]")
+        elem.click()
+        elem = self.driver.find_element_by_class_name('w3-input w3-border w3-margin-bottom')
+        elem.send_keys('test')
+        
+        elem = self.driver.find_element_by_class_name('w3-input w3-border')
+        elem.send_keys('pw')
+        
+        elem = self.driver.find_element_by_id('submit')
+        elem.click() 
+        
+        elem = self.driver.find_element_by_id('navbar_searchbox')
+        elem.send_keys('xyz')
+        elem.submit()
+        
+        
+        elem = self.driver.find_element_by_xpath("//a[contains(text(),'No Matching Result.')]")
+        assert elem.text == 'No Matching Result.'
 
     def tearDown(self):
         self.driver.quit
